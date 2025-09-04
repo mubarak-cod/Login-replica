09b87fe519096bab83f380763cabbcd9696c40ba
mubarak-cod <ososanyaola8@gmail.com>
2025-09-05 00:31:50 +0100

    updated code

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
 
   return (
 <div className="flex items-center justify-center min-h-screen bg-gray-900">
-  <div className="bg-gray-900 text-white p-8 rounded-lg w-96 shadow-2xl shadow-white/70">
+  <div className="bg-gray-900 text-white p-8 rounded-lg w-96 shadow-2xl shadow-white/20">
 
         {/* Title */}
         <h1 className="text-3xl font-bold mb-6 text-center">
